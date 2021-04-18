88e5b1db39d5c0ad1c8c41baf75b5f3ca7126057
Anh-Toine <Nick1234567890a@gmail.com>
2021-04-18 11:32:32 -0400

    Added 4 Dockerfile files

--- microservices/product-composite-service/src/main/java/com/nguyen/microservices/composite/product/integrationlayer/ProductCompositeIntegration.java
+++ microservices/product-composite-service/src/main/java/com/nguyen/microservices/composite/product/integrationlayer/ProductCompositeIntegration.java
@@ -13,9 +13,11 @@ import com.nguyen.utils.http.HttpErrorInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
